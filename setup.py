5f4e2cba0f7fe35ee25c91fa8653ba14e0b7923a
David Straub <david.straub@tum.de>
2018-03-16 17:13:08 +0100

    setup, travis, manifest

--- setup.py
+++ setup.py
@@ -1,29 +1,24 @@
 from setuptools import setup, find_packages
 
-setup(name='smeftrunner',
-      version='2.1',
-      author='Xuanyou Pan, David M. Straub',
-      author_email='xuanyou.pan@tum.de, david.straub@tum.de',
-      url='https://github.com/DsixTools/python-smeftrunner',
-      description='A Python package for the renormalization group evolution in the Standard Model Effective Field Theory (SMEFT).',
-      long_description="""A Python package for the renormalization group
-      evolution in the Standard Model Effective Field Theory (SMEFT).
-      Based on the SMEFTrunner module of the DSixTools Mathematica package
-      by Alejandro Celis, Javier Fuentes-Martín, Avelino Vicente,
-      and Javier Virto (arXiv:1704.04504). RGEs based on arXiv:1308.2627,
-      arXiv:1310.4838, and arXiv:1312.2014 by Rodrigo Alonso,
-      Elizabeth Jenkins, Aneesh Manohar, and Michael Trott.""",
+setup(name='wilson',
+      version='0.1',
+      author='Jason Aebischer, David M. Straub',
+      author_email='jason.aebischer@tum.de, david.straub@tum.de',
+      url='https://github.com/wilsoneft/wilson',
+      description='Toolkit for effective field theories beyond the Standard Model',
       license='MIT',
       packages=find_packages(),
       package_data={
-      'smeftrunner':['tests/data/*',
-              ]
-      },
-      entry_points={
-        'console_scripts': [
-            'smeftrunner-cli = smeftrunner.cli:main',
-        ]
+      'wilson': ['smeftrunner/tests/data/*',
+                 'data/*.yml',
+                 'data/*.yaml',
+                 'data/*.json',
+                 'wetrunner/tests/data/*',
+                 ],
       },
       install_requires=['scipy>=1.0', 'numpy', 'pylha>=0.2', 'pyyaml',
-                        'ckmutil>=0.3', 'wcxf'],
+                        'ckmutil>=0.3', 'wcxf>=1.2', 'rundec>=0.5'],
+      extras_require={
+            'testing': ['nose', 'smeftrunner'],
+      },
     )
