3e46cfd3f387108eab7f8af2506a4c4af6e47e7d
David Straub <david.straub@tum.de>
2018-03-20 11:10:56 +0100

    Move methods from flavio.WilsonCoefficients to wilson.Wilson

--- wilson/classes.py
+++ wilson/classes.py
@@ -1,4 +1,77 @@
+from wilson.run.smeft import SMEFT
+from wilson.run.wet import WETrunner
+
+
 class Wilson(object):
     """Wilson."""
     def __init__(self, wc):
         self.wc = wc
+        self._cache = {}
+
+    def _repr_markdown_(self):
+        r_wcxf = self.wc._repr_markdown_()
+        r_wcxf = '\n'.join(r_wcxf.splitlines()[2:])  # remove WCxf heading
+        md = "## `wilson` coefficients\n\n"
+        md += "### Initial values:\n\n"
+        md += r_wcxf
+        return md
+
+    def match_run(self, eft, basis, scale, sectors='all'):
+        """Run the Wilson coefficients to a different scale
+        (and possibly different EFT)
+        and return them as `wcxf.WC` instance."""
+        scale_ew = 91.1876
+        mb = 4.2
+        mc = 1.3
+        if self.wc.basis == basis and self.wc.eft == eft and scale == self.wc.scale:
+            return self.wc  # nothing to do
+        if self.wc.eft == 'SMEFT':
+            smeft = SMEFT(self.wc)
+            if eft == 'SMEFT':
+                # if input and output EFT ist SMEFT, just run.
+                return smeft.run(scale)
+            else:
+                # if SMEFT -> WET-x: match to WET at the EW scale
+                wc_ew = smeft.run(scale_ew).match('WET', 'Bern')
+                wet = WETrunner(wc_ew)
+        elif self.wc.eft in ['WET', 'WET-4', 'WET-3']:
+            wet = WETrunner(self.wc.translate('Bern'))
+        else:
+            raise ValueError("Input EFT {} unknown or not supported".format(self.wc.eft))
+        if eft == wet.eft:  # just run
+            return wet.run(scale, sectors=sectors).translate(basis)
+        elif eft == 'WET-4' and wet.eft == 'WET':  # match at mb
+            wc_mb = wet.run(mb, sectors=sectors).match('WET-4', 'Bern')
+            wet4 = WETrunner(wc_mb)
+            return wet4.run(scale, sectors=sectors).translate(basis)
+        elif eft == 'WET-3' and wet.eft == 'WET-4':  # match at mc
+            wc_mc = wet.run(mc, sectors=sectors).match('WET-3', 'Bern')
+            wet3 = WETrunner(wc_mc)
+            return wet3.run(scale, sectors=sectors).translate(basis)
+        elif eft == 'WET-3' and wet.eft == 'WET':  # match at mb and mc
+            wc_mb = wet.run(mb, sectors=sectors).match('WET-4', 'Bern')
+            wet4 = WETrunner(wc_mb)
+            wc_mc = wet4.run(scale, sectors=sectors).match('WET-3', 'Bern')
+            wet3 = WETrunner(wc_mc)
+            return wet3.run(scale, sectors=sectors).translate(basis)
+        else:
+            raise ValueError("Running from {} to {} not implemented".format(wet.eft, eft))
+        self._set_cache(wcxf_sector, scale, eft, basis, wc_out_dict)
+
+    def _get_from_cache(self, sector, scale, eft, basis):
+        """Try to load a set of Wilson coefficients from the cache, else return
+        None."""
+        try:
+            return self._cache[eft][scale][basis][sector]
+        except KeyError:
+            return None
+
+    def _set_cache(self, sector, scale, eft, basis, wc_out):
+        if eft not in self._cache:
+            self._cache[eft] = {scale: {basis: {sector: wc_out}}}
+        elif scale not in self._cache[eft]:
+            self._cache[eft][scale] = {basis: {sector: wc_out}}
+        elif basis not in self._cache[eft][scale]:
+            self._cache[eft][scale][basis] = {sector: wc_out}
+        else:
+            self._cache[eft][scale][basis][sector] = wc_out
--- wilson/test_wilson.py
+++ wilson/test_wilson.py
@@ -2,6 +2,7 @@ import unittest
 import wcxf
 import wilson
 import numpy as np
+import pkgutil
 
 
 np.random.seed(235)
@@ -19,7 +20,58 @@ def get_random_wc(eft, basis, scale, cmax=1e-6):
     return wcxf.WC(eft, basis, scale, wcxf.WC.dict2values(_wc))
 
 
+par = {
+    'm_Z': 91.1876,
+    'm_b': 4.18,
+    'm_d': 4.8e-3,
+    'm_s': 0.095,
+    'm_t': 173.3,
+    'm_c': 1.27,
+    'm_u': 2.3e-3,
+    'alpha_e': 1/127.944,
+    'alpha_s': 0.1185,
+    'GF': 1.1663787e-5,
+}
+
+
 class TestWilson(unittest.TestCase):
     def test_class(self):
         wc = wcxf.WC('SMEFT', 'Warsaw', 1000, {'qd1_1123': 1})
-        wi = wilson.Wilson(wc)
+        wilson.Wilson(wc)
+
+    def test_set_initial_wcxf(self):
+        test_file = pkgutil.get_data('wilson', 'data/tests/wcxf-flavio-example.yml')
+        wcxf_wc = wcxf.WC.load(test_file.decode('utf-8'))
+        wcxf_wc.validate()
+        wilson_wc = wilson.Wilson(wcxf_wc)
+        wc_out = wilson_wc.match_run('WET', 'flavio', 160, 'sb')
+        self.assertEqual(wc_out.dict['C9_bsee'], -1+0.01j)
+        self.assertEqual(wc_out.dict['C9p_bsee'], 0.1)
+        self.assertEqual(wc_out.dict['C10_bsee'], 0.05j)
+        self.assertEqual(wc_out.dict['C10p_bsee'], 0.1-0.3j)
+        self.assertEqual(wc_out.dict.get('CS_bsee', 0), 0)
+
+    def test_set_initial_wcxf_minimal(self):
+        for eft in ['WET', 'WET-4', 'WET-3']:
+            wc = wcxf.WC(eft, 'flavio', 120, {'CVLL_sdsd': {'Im': 1}})
+            ww = wilson.Wilson(wc)
+            self.assertEqual(ww.match_run(eft, 'flavio', 120, 'sdsd').dict['CVLL_sdsd'], 1j)
+            pf = 4 * par['GF'] / np.sqrt(2)
+            wc = wcxf.WC(eft, 'Bern', 120, {'1dsds': {'Im': 1/pf}})
+            ww = wilson.Wilson(wc)
+            self.assertAlmostEqual(ww.match_run(eft, 'flavio', 120, 'sdsd').dict['CVLL_sdsd'], 1j)
+
+    def tets_repr(self):
+        wc = wilson.Wilson(wc)
+        wc._repr_markdown_()
+        wc.set_initial({'C7_bs': -0.1}, 5)
+        wc._repr_markdown_()
+
+    def test_run_wcxf(self):
+        for eft in [('WET', 'WET', 120, 120), ('WET', 'WET-4', 120, 3), ('WET', 'WET-3', 120, 2),
+                    ('WET-4', 'WET-4', 3, 2), ('WET-4', 'WET-3', 3, 2),
+                    ('WET-3', 'WET-3', 2, 1), ]:
+            wc = wcxf.WC(eft[0], 'flavio', eft[2], {'CVLL_sdsd': {'Im': 1}})
+            ww = wilson.Wilson(wc)
+            wc_out = ww.match_run(eft[1], 'flavio', eft[3])
+            wc_out.validate()
