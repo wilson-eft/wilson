1b63447594967118ca157db83fa871050fe2df76
David Straub <david.straub@tum.de>
2018-04-12 09:11:18 +0200

    Removed bases (will be added to wcxf-bases repo)

--- wilson/__init__.py
+++ wilson/__init__.py
@@ -5,17 +5,3 @@ from . import match
 from . import translate
 from . import parameters
 from .classes import Wilson
-
-# Temporary: modified bases
-
-import os
-import glob
-import wcxf
-
-_root = os.path.abspath(os.path.dirname(__file__))
-all_bases = glob.glob(os.path.join(_root, 'data/bases', '*.basis.yml'))
-
-
-for basis in all_bases:
-    with open(basis, 'r') as f:
-        wcxf.Basis.load(f)
