54a9ce8108fe87d19abd01c7b206cb15528eecce
David Straub <david.straub@tum.de>
2018-07-09 16:46:48 +0200

    Account for symmetry factors in SMEFT running.
    Rather than modifying the beta functions, this is done by
    rescaling the Wilson coefficients prior to and after running.

--- wilson/run/smeft/rge.py
+++ wilson/run/smeft/rge.py
@@ -7,18 +7,53 @@ from math import pi, log
 from scipy.integrate import solve_ivp
 from wilson.run.smeft.beta import C_array2dict
 from wilson.translate.smeft import arrays2wcxf
+from wilson.util.smeftutil import C_symm_keys
 import numpy as np
+
+
+# computing the scale vector required for rescale_dict below
+# initialize with factor 1
+_d_4 = np.zeros((3,3,3,3))
+_d_6 = np.zeros((3,3,3,3))
+for i in range(3):
+    for j in range(3):
+        for k in range(3):
+            for l in range(3):
+                # class 4: symmetric under interachange of currents
+                _d_4[i, j, k, l] = len(set([(i, j, k, l), (k, l, i, j)]))
+                # class 4: symmetric under interachange of currents + Fierz
+                _d_6[i, j, k, l] = len(set([(i, j, k, l), (k, l, i, j), (k, j, i, l), (i, l, k, j)]))
+
+
+_scale_dict = C_array2dict(np.ones(9999))
+for k in C_symm_keys[4]:
+    _scale_dict[k] = _d_4
+for k in C_symm_keys[6]:
+    _scale_dict[k] = _d_6
+
+
+def scale_dict(C):
+    """To account for the fact that arXiv:1312.2014 uses a flavour
+    non-redundant basis in contrast to WCxf, symmetry factors of two have to
+    be introduced in several places for operators that are symmetric
+    under the interchange of two currents."""
+    return {k: _scale_dict[k] * v for k, v in C.items()}
+
+
+def unscale_dict(C):
+    """Undo the scaling applied in `scale_dict`."""
+    return {k: 1 / _scale_dict[k] * v for k, v in C.items()}
 
 
 def smeft_evolve_leadinglog(C_in, scale_in, scale_out, newphys=True):
     """Solve the SMEFT RGEs in the leading log approximation.
 
     Input C_in and output C_out are dictionaries of arrays."""
-    C_out = deepcopy(C_in)
-    b = beta.beta(C_in, newphys=newphys)
+    C_out = scale_dict(deepcopy(C_in))
+    b = beta.beta(C_out, newphys=newphys)
     for k, C in C_out.items():
         C_out[k] = C + b[k] / (16 * pi**2) * log(scale_out / scale_in)
-    return C_out
+    return unscale_dict(C_out)
 
 
 def _smeft_evolve(C_in, scale_in, scale_out, newphys=True, **kwargs):
@@ -26,7 +61,7 @@ def _smeft_evolve(C_in, scale_in, scale_out, newphys=True, **kwargs):
     def fun(t0, y):
         return beta.beta_array(C=beta.C_array2dict(y.view(complex)),
                                newphys=newphys).view(float) / (16 * pi**2)
-    y0 = beta.C_dict2array(C_in).view(float)
+    y0 = beta.C_dict2array(scale_dict(C_in)).view(float)
     sol = solve_ivp(fun=fun,
                     t_span=(log(scale_in), log(scale_out)),
                     y0=y0, **kwargs)
@@ -38,7 +73,7 @@ def smeft_evolve(C_in, scale_in, scale_out, newphys=True, **kwargs):
 
     Input C_in and output C_out are dictionaries of arrays."""
     sol = _smeft_evolve(C_in, scale_in, scale_out, newphys=newphys, **kwargs)
-    return beta.C_array2dict(sol.y[:, -1].view(complex))
+    return unscale_dict(beta.C_array2dict(sol.y[:, -1].view(complex)))
 
 
 def smeft_evolve_continuous(C_in, scale_in, scale_out, newphys=True, **kwargs):
@@ -51,7 +86,7 @@ def smeft_evolve_continuous(C_in, scale_in, scale_out, newphys=True, **kwargs):
     def _rge_solution(scale):
         t = log(scale)
         y = sol.sol(t).view(complex)
-        yd = C_array2dict(y)
+        yd = unscale_dict(C_array2dict(y))
         yw = arrays2wcxf(yd)
         return yw
     def rge_solution(scale):
