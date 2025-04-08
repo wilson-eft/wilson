ddac6eefc1d393bf781d05b786ca0b31f030f943
Peter Stangl <peterstangl@users.noreply.github.com>
2025-04-08 12:44:19 +0200

    update SMEFT sectors (#125)

--- wilson/wcxf/test_smeft.py
+++ wilson/wcxf/test_smeft.py
@@ -6,15 +6,29 @@ import json
 import pkgutil
 from wilson import wcxf
 
+sectors_dBdL0 = [
+    'dB=de=dmu=dtau=0',
+    'mue',
+    'taue',
+    'mutau',
+    'muemue',
+    'etauemu',
+    'muemutau',
+    'tauetaue',
+    'tauetaumu',
+    'taumutaumu',
+]
+
 class TestSMEFT(unittest.TestCase):
     def test_smeft_number(self):
         """Test the number of B and L conserving coefficients in SMEFT"""
         basis = wcxf.Basis['SMEFT', 'Warsaw']
-        sector = basis.sectors['dB=dL=0']
         count=0
-        for wc in sector:
-            if 'real' not in sector[wc] or not sector[wc]['real']:
-                count +=2 # complex coeffs
-            else:
-                count +=1 # real coeffs
+        for sector_name in sectors_dBdL0:
+            sector = basis.sectors[sector_name]
+            for wc in sector:
+                if 'real' not in sector[wc] or not sector[wc]['real']:
+                    count +=2 # complex coeffs
+                else:
+                    count +=1 # real coeffs
         self.assertEqual(count, 2499) # no. of B and L conserving coefficients
